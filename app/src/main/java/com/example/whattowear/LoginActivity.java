27c0412c1227c856be9704dc83a344b1a4b64c00
Aaron de los Santos <hansaaron_d@yahoo.com>
2022-06-14 15:49:58 -0700

    Removed moveToMainActivity function and removed parameters from loginUser in login activity

--- app/src/main/java/com/example/whattowear/LoginActivity.java
+++ app/src/main/java/com/example/whattowear/LoginActivity.java
@@ -18,7 +18,7 @@ public class LoginActivity extends AppCompatActivity {
     private EditText username_edittext;
     private EditText password_edittext;
     private Button login_button;
-    private Button signup_button;
+    private Button go_to_signup_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,39 +28,40 @@ public class LoginActivity extends AppCompatActivity {
         username_edittext = findViewById(R.id.username_edittext);
         password_edittext = findViewById(R.id.password_edittext);
         login_button = findViewById(R.id.login_button);
-        signup_button = findViewById(R.id.signup_button);
+        go_to_signup_button = findViewById(R.id.go_to_signup_button);
 
         login_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                loginUser(username_edittext.getText().toString(), password_edittext.getText().toString());
+                loginUser();
             }
         });
 
-        signup_button.setOnClickListener(new View.OnClickListener() {
+        go_to_signup_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                // Move to signup screen
+                Intent i = new Intent(LoginActivity.this, SignupActivity.class);
+                startActivity(i);
             }
         });
     }
 
-    public void loginUser(String username, String password) {
+    public void loginUser() {
         Log.i(TAG, "Attempting to login user");
+        String username = username_edittext.getText().toString();
+        String password = password_edittext.getText().toString();
 
         ParseUser.logInInBackground(username, password, (user, e) -> {
             if (user != null) {
                 Toast.makeText(this, "Successfully logged in.", Toast.LENGTH_SHORT).show();
+
                 // Move to main activity
-                moveToMainActivity();
+                Intent i = new Intent(this, MainActivity.class);
+                startActivity(i);
             } else {
                 Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
             }
         });
-    }
-
-    private void moveToMainActivity() {
-        Intent i = new Intent(this, MainActivity.class);
-        startActivity(i);
     }
 }
