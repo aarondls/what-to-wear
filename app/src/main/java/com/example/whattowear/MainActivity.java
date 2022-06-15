aa84e2f865a5fbd171ad1aeaf9f8db19d17b997f
Aaron de los Santos <hansaaron_d@yahoo.com>
2022-06-15 10:25:31 -0700

    Implemented location services runtime permissions

--- app/src/main/java/com/example/whattowear/MainActivity.java
+++ app/src/main/java/com/example/whattowear/MainActivity.java
@@ -1,14 +1,69 @@
 package com.example.whattowear;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.List;
 
+import pub.devrel.easypermissions.AppSettingsDialog;
+import pub.devrel.easypermissions.EasyPermissions;
+
+public class MainActivity extends AppCompatActivity implements EasyPermissions.PermissionCallbacks {
+    public static final int PERMISSIONS_REQUEST_CODE = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // request permissions if no permission given (can be moved to a button click or anywhere)
+        if (!hasLocationPermissions()) {
+            requestLocationPermissions();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        // Forward results to EasyPermissions
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
+    }
+
+    /**
+     * Can be used to check if location permissions have been granted
+     * @return
+     */
+    private boolean hasLocationPermissions() {
+        return EasyPermissions.hasPermissions(this, Manifest.permission.ACCESS_FINE_LOCATION);
+    }
+
+    private void requestLocationPermissions() {
+        EasyPermissions.requestPermissions(
+                this,
+                "This application requires location services to auto detect your location.",
+                PERMISSIONS_REQUEST_CODE,
+                Manifest.permission.ACCESS_FINE_LOCATION);
+    }
+
+    @Override
+    public void onPermissionsGranted(int requestCode, @NonNull List<String> perms) {
+        Toast.makeText(this, "Location services permission granted!", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onPermissionsDenied(int requestCode, @NonNull List<String> perms) {
+        Toast.makeText(this, "Location services permission denied!", Toast.LENGTH_SHORT).show();
+
+        // Check whether the user denied any permissions and checked "NEVER ASK AGAIN."
+        // This will display a dialog directing them to enable the permission in app settings.
+        if (EasyPermissions.somePermissionPermanentlyDenied(this, perms)) {
+            new AppSettingsDialog.Builder(this).build().show();
+        } else {
+            requestLocationPermissions();
+        }
     }
 }
