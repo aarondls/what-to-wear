fe0342e95ebf66039e8ea7a96e6410392b002f54
Aaron de los Santos <hansaaron_d@yahoo.com>
2022-06-17 17:40:06 -0700

    Create classes for describing weather data
    Created the classes Conditions, Forecast, and Weather to store the received weather information from the openweather API.

---
+++ app/src/main/java/com/example/whattowear/models/Conditions.java
@@ -0,0 +1,29 @@
+package com.example.whattowear.models;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Conditions {
+    private static final String IMG_URL_BEGIN = "https://openweathermap.org/img/wn/";
+    private static final String IMG_URL_END = "@2x.png";
+
+    private int weatherConditionsID;
+    private String conditionSummary;
+    private String conditionDescription;
+    private String conditionIconLink;
+
+    public static Conditions fromJson(JSONObject jsonObject) throws JSONException {
+        Conditions conditions = new Conditions();
+
+        conditions.weatherConditionsID = jsonObject.getInt("id");
+        conditions.conditionSummary = jsonObject.getString("main");
+        conditions.conditionDescription = jsonObject.getString("description");
+        conditions.conditionIconLink = IMG_URL_BEGIN + jsonObject.getString("icon") + IMG_URL_END;
+
+        return conditions;
+    }
+
+    public String getConditionIconLink() {
+        return conditionIconLink;
+    }
+}
